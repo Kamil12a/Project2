f76be75fee9fa8c01a99ccaffcf0cb9ccdc29238
Kamil-PLewka <kamilplewka12a@wp.pl>
2020-12-20 19:23:14 +0100

    change names

---
+++ pikachu_game_falling_food/Document_files/exercises.js
@@ -0,0 +1,53 @@
+// Zad 1
+function sumElements(array) {
+  let lengthOfArray=array.length
+  
+  let amountOfNumber=0;
+  for(index=0;index<lengthOfArray;index++){
+    if(Number.isInteger(array[index])){
+      amountOfNumber=amountOfNumber+array[index]
+    }
+  }
+  console.log("ilość elementów: " +lengthOfArray)
+  console.log("suma liczb: "+amountOfNumber)
+}
+let array=[['5', '0', 9, 3, 2, 1, '9', 6, 7]]
+sumElements(array)
+function sumNumbers() {}
+
+// Zad 2
+function getInitials() {}
+// Zad 3
+function sumPositives() {}
+
+//BONUS
+function countPositivesAndSumNegatives() {
+  // returns [positivesCount, negativeSum]
+}
+
+// Zad 4
+function praiseHardworkersBashLazyStudents() {}
+// Zad 5
+function sumArrays() {}
+// Zad 6
+function canYouPlayBanjo() {}
+// Zad 7
+function reverseString() {}
+// Zad 8
+function enough() {}
+// Zad 9
+function multiplyTable() {}
+// Zad 10
+function canDivideBy() {}
+// Zad 11
+function ageDifferences() {}
+// Zad 12
+function gimmeMiddleIndex() {}
+// Zad 13
+function whatsMySuperVillainName() {}
+// Zad 14
+function checkExam() {}
+// Zad 15
+function getNiceNames(){}
+
+function getNaughtNames() {}
